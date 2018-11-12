2ef818600758a868db917a83ee608310e15c7443
StefanBern <sbstefanbern@gmail.com>
2018-11-12 15:17:00 +0100

    header meta info

--- header.php
+++ header.php
@@ -1,12 +1,12 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML>
 <html lang="en" dir="ltr">
   <head>
-    <meta charset="utf-8">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
+    <meta name="description" content="">
     <?php wp_head(); ?>
   </head>
-
   <body>
     <?php wp_nav_menu(array('theme_location'=>'primary')); ?>
-
-    <h1>Hvad så bøsser</h1>
+    <h1>Antraxen</h1>
