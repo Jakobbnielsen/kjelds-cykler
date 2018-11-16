f1d326099d4f9d9971752dfca235fec3f62dc617
StefanBern <sbstefanbern@gmail.com>
2018-11-16 13:58:55 +0100

    menu styling and logo

--- header.php
+++ header.php
@@ -5,13 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php bloginfo('title'); ?></title>
     <meta name="description" content="">
+    <link href="https://fonts.googleapis.com/css?family=Leckerli+One" rel="stylesheet">
     <?php wp_head(); ?>
   </head>
   <body>
     <main>
       <header id="menu">
+        <div class="logo">
+          <a href="front-page.php"><img src="http://localhost:8888/wordpress/wp-content/uploads/2018/11/logo.png" alt="Logo"></a>
+        </div>
         <?php wp_nav_menu(array('theme_location'=>'primary')); ?>
       </header>
       <section id="banner">
-        <p>header</p>
       </section>
